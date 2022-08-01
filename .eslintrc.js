14401d0b48caf582dd1a4a90ca07f9ad25436187
vlad.piatyhor <vlad.piatyhor@gmail.com>
2022-08-01 15:45:02 +0300

    add eslint settings

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.export = {
+  env: {
+    es6: true,
+  },
+  rules: {
+    strict: ["error", "global"],
+    "no-var": "error",
+    "prefer-cont": "error",
+    "one-var": ["error", "never"],
+    camelcase: "error",
+    "no-unused-vars": "error",
+    "no-multi-assign": "error",
+    quotes: ["error", "single"],
+    "no-array-constructor": "error",
+    "no-new-object": "error",
+    "quote-props": ["error", "as-needed"],
+    "no-new-wrappers": "error",
+    "no-extra-boolean-cast": "error",
+    eqeqeq: "error",
+    yoda: "error",
+    "no-unneeded-ternary": "error",
+    "no-nested-ternary": "error",
+  },
+};
